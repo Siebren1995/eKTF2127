5637ef2f608a653a3e01d40c466dd84ea13f63aa
Siebren Vroegindeweij <Siebren@dhcp-145-52-152-150.wireless.hhs.nl>
2016-05-26 15:52:30 +0200

    Add send coordinates to the input system from the Linux kernel and change some name to match the correct driver

--- eKTF2127.c
+++ eKTF2127.c
@@ -25,16 +25,17 @@
 #include <linux/module.h>
 #include <linux/of.h>
 
-#define ICN8318_REG_POWER		4
-#define ICN8318_REG_TOUCHDATA		16
+#define EKTF2127_REG_POWER		4
+#define EKTF2127_REG_TOUCHDATA		16
 
-#define ICN8318_POWER_ACTIVE		0
-#define ICN8318_POWER_MONITOR		1
-#define ICN8318_POWER_HIBERNATE		2
+#define EKTF2127_POWER_ACTIVE		0
+#define EKTF2127_POWER_MONITOR		1
+#define EKTF2127_POWER_HIBERNATE	2
 
-#define ICN8318_MAX_TOUCHES		5
+#define EKTF2127_MAX_TOUCHES		5
+#define eKTF2127_MAX_TOUCHES		5
 
-struct icn8318_touch {
+struct ektf2127_touch {
 	__u8 slot;
 //__be16 x;
 //__be16 y;
@@ -45,19 +46,19 @@ struct icn8318_touch {
 	__u8 pressure;	/* Seems more like finger width then pressure really */
 	__u8 event;
 /* The difference between 2 and 3 is unclear */
-#define ICN8318_EVENT_NO_DATA	1 /* No finger seen yet since wakeup */
-#define ICN8318_EVENT_UPDATE1	2 /* New or updated coordinates */
-#define ICN8318_EVENT_UPDATE2	3 /* New or updated coordinates */
-#define ICN8318_EVENT_END	4 /* Finger lifted */
+#define EKTF2127_EVENT_NO_DATA	1 /* No finger seen yet since wakeup */
+#define EKTF2127_EVENT_UPDATE1	2 /* New or updated coordinates */
+#define EKTF2127_EVENT_UPDATE2	3 /* New or updated coordinates */
+#define EKTF2127_EVENT_END	4 /* Finger lifted */
 } __packed;
 
-struct icn8318_touch_data {
+struct ektf2127_touch_data {
 	__u8 softbutton;
 	__u8 touch_count;
-	struct icn8318_touch touches[ICN8318_MAX_TOUCHES];
+	struct ektf2127_touch touches[eKTF2127_MAX_TOUCHES];
 } __packed;
 
-struct icn8318_data {
+struct ektf2127_data {
 	struct i2c_client *client;
 	struct input_dev *input;
 	struct gpio_desc *power_gpios;
@@ -68,10 +69,10 @@ struct icn8318_data {
 	bool swap_x_y;
 };
 
-static int icn8318_read_touch_data(struct i2c_client *client,
-				   struct icn8318_touch_data *touch_data)
+static int ektf2127_read_touch_data(struct i2c_client *client,
+				   struct ektf2127_touch_data *touch_data)
 {
-	u8 reg = ICN8318_REG_TOUCHDATA;
+	u8 reg = EKTF2127_REG_TOUCHDATA;
 	struct i2c_msg msg[2] = {
 		{
 			.addr = client->addr,
@@ -81,7 +82,7 @@ static int icn8318_read_touch_data(struct i2c_client *client,
 		{
 			.addr = client->addr,
 			.flags = I2C_M_RD,
-			.len = sizeof(struct icn8318_touch_data),
+			.len = sizeof(struct ektf2127_touch_data),
 			.buf = (u8 *)touch_data
 		}
 	};
@@ -89,10 +90,10 @@ static int icn8318_read_touch_data(struct i2c_client *client,
 	return i2c_transfer(client->adapter, msg, 2);
 }
 
-static inline bool icn8318_touch_active(u8 event)
+static inline bool ektf2127_touch_active(u8 event)
 {
-	return (event == ICN8318_EVENT_UPDATE1) ||
-	       (event == ICN8318_EVENT_UPDATE2);
+	return (event == EKTF2127_EVENT_UPDATE1) ||
+	       (event == EKTF2127_EVENT_UPDATE2);
 }
 
 static int get_coordinates(int *x, int *y, char *buf)
@@ -111,17 +112,18 @@ static int get_coordinates(int *x, int *y, char *buf)
         return 0;
 }
 
-static irqreturn_t icn8318_irq(int irq, void *dev_id)
+static irqreturn_t ektf2127_irq(int irq, void *dev_id)
 {
-	struct icn8318_data *data = dev_id;
+	struct ektf2127_data *data = dev_id;
 	struct device *dev = &data->client->dev;
-	struct icn8318_touch_data touch_data;
+	struct ektf2127_touch_data touch_data;
 	char buff[25];
-	int i, ret, x[5], y[5], index, count;
+	int i, ret, x, y, index, count;
 
-//ret = icn8318_read_touch_data(data->client, &touch_data);
+//ret = ektf2127_read_touch_data(data->client, &touch_data);
 
 	ret = i2c_master_recv(data->client, buff, 25);
+
 //*buff2[0] = be16_to_cpu(buff[0]);
 	//dev_err(dev, "Buffer: %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n", buff[0], buff[1], buff[2], buff[3], buff[4], buff[5], buff[6], buff[7], buff[8], buff[9], buff[10]);
 
@@ -129,15 +131,17 @@ static irqreturn_t icn8318_irq(int irq, void *dev_id)
 		
 		if (count > 0) {
 		        dev_err(dev, "Number of touches: %d \n", count);
+
 		        for (i = 0; i < count; i++) {
 		                index = 2 + i * 3;
-		                ret = get_coordinates(&x[i], &y[i], &buff[index]);
-
-		                dev_err(dev, "x%d: %d, y%d: %d\n", i, x[i], i, y[i]);
+		                ret = get_coordinates(&x, &y, &buff[index]);
+				if(ret != 0){
+					dev_err(dev, "Error reading touch data: %d\n", ret);
+					return IRQ_HANDLED;
+				}
+				dev_err(dev, "x%d: %d, y%d: %d\n", i, x, i, y);
 		        }
 		}
-
-	get_coordinates(&x, &y, buff);
 	
 	/*dev_err(dev, "Amount of touches %d", touch_data.touch_count);
 	x = be16_to_cpu(touch_data.touches[0].x);
@@ -145,41 +149,38 @@ static irqreturn_t icn8318_irq(int irq, void *dev_id)
 	dev_err(dev, "X: %d", x);
 	dev_err(dev, "Y: %d", y);*/
 
-	if (ret < 0) {
-		dev_err(dev, "Error reading touch data: %d\n", ret);
-		return IRQ_HANDLED;
-	}
-
-	if (touch_data.softbutton) {
+//if (touch_data.softbutton) {
 		/*
 		 * Other data is invalid when a softbutton is pressed.
 		 * This needs some extra devicetree bindings to map the icn8318
 		 * softbutton codes to evdev codes. Currently no known devices
 		 * use this.
 		 */
-		return IRQ_HANDLED;
-	}
+//return IRQ_HANDLED;
+//}
 
-	touch_data.touch_count = count;
+	//touch_data.touch_count = count;
 
-	if (touch_data.touch_count > ICN8318_MAX_TOUCHES) {
+	/*if (touch_data.touch_count > EKTF2127_MAX_TOUCHES) {
 		dev_warn(dev, "Too much touches %d > %d\n",
-			 touch_data.touch_count, ICN8318_MAX_TOUCHES);
-		touch_data.touch_count = ICN8318_MAX_TOUCHES;
+			 touch_data.touch_count, EKTF2127_MAX_TOUCHES);
+		touch_data.touch_count = EKTF2127_MAX_TOUCHES;
+	}*/
+
+	if(count > eKTF2127_MAX_TOUCHES){
+		dev_err(dev, "Too many touches %d > %d\n", count, eKTF2127_MAX_TOUCHES);
+		count = eKTF2127_MAX_TOUCHES;
 	}
 
-	for (i = 0; i < touch_data.touch_count; i++) {
+	for (i = 0; i < count; i++) {
 
-		touch_data.touches[i].x = x[i];
-		touch_data.touches[i].y = y[i];
-
-		struct icn8318_touch *touch = &touch_data.touches[i];
-		bool act = icn8318_touch_active(touch->event);
+		/*struct ektf2127_touch *touch = &touch_data.touches[i];
+		bool act = ektf2127_touch_active(touch->event);
 
 		input_mt_slot(data->input, touch->slot);
 		input_mt_report_slot_state(data->input, MT_TOOL_FINGER, act);
 		if (!act)
-			continue;
+			continue;*/
 
 		//x = be16_to_cpu(touch->x);
 		//y = be16_to_cpu(touch->y);
@@ -190,13 +191,19 @@ static irqreturn_t icn8318_irq(int irq, void *dev_id)
 		if (data->invert_y)
 			y = data->max_y - y;*/
 
-		if (!data->swap_x_y) {
+		/*if (!data->swap_x_y) {
 			input_event(data->input, EV_ABS, ABS_MT_POSITION_X, x[i]);
 			input_event(data->input, EV_ABS, ABS_MT_POSITION_Y, y[i]);
 		} else {
 			input_event(data->input, EV_ABS, ABS_MT_POSITION_X, y[i]);
 			input_event(data->input, EV_ABS, ABS_MT_POSITION_Y, x[i]);
-		}
+		}*/
+
+		input_mt_slot(data->input, 0);
+		input_mt_report_slot_state(data->input, MT_TOOL_FINGER, true);
+
+		input_event(data->input, EV_ABS, ABS_MT_POSITION_X, x);
+		input_event(data->input, EV_ABS, ABS_MT_POSITION_Y, y);
 	}
 
 	input_mt_sync_frame(data->input);
@@ -205,9 +212,9 @@ static irqreturn_t icn8318_irq(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-static int icn8318_start(struct input_dev *dev)
+static int ektf2127_start(struct input_dev *dev)
 {
-	struct icn8318_data *data = input_get_drvdata(dev);
+	struct ektf2127_data *data = input_get_drvdata(dev);
 
 	enable_irq(data->client->irq);
 	gpiod_set_value_cansleep(data->power_gpios, 1);
@@ -215,50 +222,50 @@ static int icn8318_start(struct input_dev *dev)
 	return 0;
 }
 
-static void icn8318_stop(struct input_dev *dev)
+static void ektf2127_stop(struct input_dev *dev)
 {
-	struct icn8318_data *data = input_get_drvdata(dev);
+	struct ektf2127_data *data = input_get_drvdata(dev);
 
 	disable_irq(data->client->irq);
-	i2c_smbus_write_byte_data(data->client, ICN8318_REG_POWER,
-				  ICN8318_POWER_HIBERNATE);
+	i2c_smbus_write_byte_data(data->client, EKTF2127_REG_POWER,
+				  EKTF2127_POWER_HIBERNATE);
 	gpiod_set_value_cansleep(data->power_gpios, 0);
 }
 
 #ifdef CONFIG_PM_SLEEP
-static int icn8318_suspend(struct device *dev)
+static int ektf2127_suspend(struct device *dev)
 {
-	struct icn8318_data *data = i2c_get_clientdata(to_i2c_client(dev));
+	struct ektf2127_data *data = i2c_get_clientdata(to_i2c_client(dev));
 
 	mutex_lock(&data->input->mutex);
 	if (data->input->users)
-		icn8318_stop(data->input);
+		ektf2127_stop(data->input);
 	mutex_unlock(&data->input->mutex);
 
 	return 0;
 }
 
-static int icn8318_resume(struct device *dev)
+static int ektf2127_resume(struct device *dev)
 {
-	struct icn8318_data *data = i2c_get_clientdata(to_i2c_client(dev));
+	struct ektf2127_data *data = i2c_get_clientdata(to_i2c_client(dev));
 
 	mutex_lock(&data->input->mutex);
 	if (data->input->users)
-		icn8318_start(data->input);
+		ektf2127_start(data->input);
 	mutex_unlock(&data->input->mutex);
 
 	return 0;
 }
 #endif
 
-static SIMPLE_DEV_PM_OPS(icn8318_pm_ops, icn8318_suspend, icn8318_resume);
+static SIMPLE_DEV_PM_OPS(ektf2127_pm_ops, ektf2127_suspend, ektf2127_resume);
 
-static int icn8318_probe(struct i2c_client *client,
+static int ektf2127_probe(struct i2c_client *client,
 			 const struct i2c_device_id *id)
 {
 	struct device *dev = &client->dev;
 	struct device_node *np = dev->of_node;
-	struct icn8318_data *data;
+	struct ektf2127_data *data;
 	struct input_dev *input;
 	u32 fuzz_x = 0, fuzz_y = 0;
 	int error;
@@ -299,8 +306,8 @@ static int icn8318_probe(struct i2c_client *client,
 
 	input->name = client->name;
 	input->id.bustype = BUS_I2C;
-	input->open = icn8318_start;
-	input->close = icn8318_stop;
+	input->open = ektf2127_start;
+	input->close = ektf2127_stop;
 	input->dev.parent = dev;
 
 	if (!data->swap_x_y) {
@@ -315,7 +322,7 @@ static int icn8318_probe(struct i2c_client *client,
 				     data->max_x, fuzz_x, 0);
 	}
 
-	error = input_mt_init_slots(input, ICN8318_MAX_TOUCHES,
+	error = input_mt_init_slots(input, eKTF2127_MAX_TOUCHES,
 				    INPUT_MT_DIRECT | INPUT_MT_DROP_UNUSED);
 	if (error)
 		return error;
@@ -324,7 +331,7 @@ static int icn8318_probe(struct i2c_client *client,
 	data->input = input;
 	input_set_drvdata(input, data);
 
-	error = devm_request_threaded_irq(dev, client->irq, NULL, icn8318_irq,
+	error = devm_request_threaded_irq(dev, client->irq, NULL, ektf2127_irq,
 					  IRQF_ONESHOT, client->name, data);
 	if (error) {
 		dev_err(dev, "Error requesting irq: %d\n", error);
@@ -332,7 +339,7 @@ static int icn8318_probe(struct i2c_client *client,
 	}
 
 	/* Stop device till opened */
-	icn8318_stop(data->input);
+	ektf2127_stop(data->input);
 
 	error = input_register_device(input);
 	if (error)
@@ -343,33 +350,33 @@ static int icn8318_probe(struct i2c_client *client,
 	return 0;
 }
 
-static const struct of_device_id icn8318_of_match[] = {
+static const struct of_device_id ektf2127_of_match[] = {
 	{ .compatible = "elan,ektf2127" },
 	{ }
 };
-MODULE_DEVICE_TABLE(of, icn8318_of_match);
+MODULE_DEVICE_TABLE(of, ektf2127_of_match);
 
 /* This is useless for OF-enabled devices, but it is needed by I2C subsystem */
-static const struct i2c_device_id icn8318_i2c_id[] = {
+static const struct i2c_device_id ektf2127_i2c_id[] = {
 	{ },
 };
-MODULE_DEVICE_TABLE(i2c, icn8318_i2c_id);
+MODULE_DEVICE_TABLE(i2c, ektf2127_i2c_id);
 
-static struct i2c_driver icn8318_driver = {
+static struct i2c_driver ektf2127_driver = {
 	.driver = {
-		//.name	= "chipone_icn8318",
 		.name	= "elan_ektf2127",
-		.pm	= &icn8318_pm_ops,
-		.of_match_table = icn8318_of_match,
+		.pm	= &ektf2127_pm_ops,
+		.of_match_table = ektf2127_of_match,
 	},
-	.probe = icn8318_probe,
-	.id_table = icn8318_i2c_id,
+	.probe = ektf2127_probe,
+	.id_table = ektf2127_i2c_id,
 };
 
-module_i2c_driver(icn8318_driver);
+module_i2c_driver(ektf2127_driver);
 
-MODULE_DESCRIPTION("ChipOne icn8318 I2C Touchscreen Driver");
-MODULE_AUTHOR("Hans de Goede <hdegoede@redhat.com>");
+MODULE_DESCRIPTION("ELAN eKTF2127 I2C Touchscreen Driver");
+MODULE_AUTHOR("Michel Verlaan");
+MODULE_AUTHOR("Siebren Vroegindeweij");
 MODULE_LICENSE("GPL");
 
 //MODULE_DESCRIPTION("ChipOne icn8318 I2C Touchscreen Driver");
